521e8d493bc636534803ff1136e924fe9a6f8c14
luisfernandob87 <luisfernandob87@gmail.com>
2022-08-24 22:05:36 -0600

    data for testing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,91 @@
-import axios from 'axios';
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const { register, handleSubmit, reset } = useForm();
 
-    const { register, handleSubmit, reset} = useForm();
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const submit = (data) => {
+    axios
+      .post(
+        "https://ecommerce-api-react.herokuapp.com/api/v1/users/login",
+        data
+      )
+      .then((res) => {
+        localStorage.setItem("token", res.data.data.token);
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          alert("Credenciales invalidas");
+        }
+      });
+    reset({
+      email: "",
+      password: "",
+    });
+  };
 
-    const submit = data =>{
-        axios.post('https://ecommerce-api-react.herokuapp.com/api/v1/users/login', data)
-        .then(res => {
-            localStorage.setItem('token', res.data.data.token)
-            navigate('/')})
-        .catch(error => {
-            if (error.response.status === 404) {
-                alert("Credenciales invalidas")
-            }
-        })
-        reset({
-            email:"",
-            password:""
-        })
-    }
+  return (
+    <div>
+      <Form
+        onSubmit={handleSubmit(submit)}
+        style={{ textAlign: "center", paddingTop: "20px" }}
+      >
+        <Form.Group
+          style={{ maxWidth: "500px" }}
+          className="mb-3"
+          controlId="formBasicEmail"
+        >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            {...register("email")}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-    return (
-        <div>
-            <Form onSubmit={handleSubmit(submit)} style={{textAlign:'center', paddingTop:'20px'}}>
-                <Form.Group style={{maxWidth:'500px'}} className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control  type="email" placeholder="Enter email" {...register("email")} />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group style={{maxWidth:'500px'}} className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" {...register("password")} />
-                </Form.Group>
-                <Button style={{background: '#ff0000', border:'none', color:'white', padding:'10px', borderRadius:'10px', marginBottom:'20px'}} variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
-    );
+        <Form.Group
+          style={{ maxWidth: "500px" }}
+          className="mb-3"
+          controlId="formBasicPassword"
+        >
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+          />
+        </Form.Group>
+        <Button
+          style={{
+            background: "#ff0000",
+            border: "none",
+            color: "white",
+            padding: "10px",
+            borderRadius: "10px",
+            marginBottom: "20px",
+          }}
+          variant="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+      <div className="testing">
+        <h4>Data for testing</h4>
+        <p>User: luis@gmail.com</p>
+        <p>Password: 1234567890</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
